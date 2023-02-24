ca420f50ca1c0fab2658deb51cf694d7b5d11709
FHA04 <farhan.lmj15@gmail.com>
2023-02-24 09:22:17 +0700

    Minggu 3

---
+++ Minggu_3/TextView/app/src/main/java/com/example/textview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.textview;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private Adapter adapter;
+    private ArrayList<Mahasiswa> mahasiswaArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addData();
+
+        recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
+
+        adapter = new Adapter(mahasiswaArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    void addData() {
+        mahasiswaArrayList = new ArrayList<>();
+        mahasiswaArrayList.add(new Mahasiswa("Dimas Maulana", "1414370309", "123456789"));
+        mahasiswaArrayList.add(new Mahasiswa("Bobon Luis", "1820468591", "128291001"));
+        mahasiswaArrayList.add(new Mahasiswa("Victor Abowo", "1649279272", "128157789"));
+        mahasiswaArrayList.add(new Mahasiswa("Sera Juan", "1614920169", "174456519"));
+    }
+}
