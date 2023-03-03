b0be6bc09ac780151a351913a53c66c094151bb8
FHA04 <farhan.lmj15@gmail.com>
2023-03-03 10:45:13 +0700

    Minggu 3

--- Minggu_3/TextView/app/src/main/java/com/example/textview/MainActivity.java
+++ Minggu_3/TextView/app/src/main/java/com/example/textview/MainActivity.java
@@ -37,5 +37,12 @@ public class MainActivity extends AppCompatActivity {
         mahasiswaArrayList.add(new Mahasiswa("Bobon Luis", "1820468591", "128291001"));
         mahasiswaArrayList.add(new Mahasiswa("Victor Abowo", "1649279272", "128157789"));
         mahasiswaArrayList.add(new Mahasiswa("Sera Juan", "1614920169", "174456519"));
+        mahasiswaArrayList.add(new Mahasiswa("Mario Luigi", "2486344269", "177456529"));
+        mahasiswaArrayList.add(new Mahasiswa("Udin Bagus", "5114106369", "174456539"));
+        mahasiswaArrayList.add(new Mahasiswa("Sina Edgar", "1114909569", "174484449"));
+        mahasiswaArrayList.add(new Mahasiswa("Fafan Mahen", "8214137169", "173566519"));
+        mahasiswaArrayList.add(new Mahasiswa("Tutung Ayib", "7414954169", "179226519"));
+        mahasiswaArrayList.add(new Mahasiswa("Kukung Miswar", "5617520169", "177736519"));
+        mahasiswaArrayList.add(new Mahasiswa("Raden Dolar", "9416340169", "174463219"));
     }
 }
