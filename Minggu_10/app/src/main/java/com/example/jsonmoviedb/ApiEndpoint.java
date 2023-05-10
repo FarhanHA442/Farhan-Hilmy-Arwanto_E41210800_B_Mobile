c29d2c43f653321b3e0273b19bf87b3097e6e8f6
FHA04 <farhan.lmj15@gmail.com>
2023-05-10 10:34:55 +0700

    minggu 10

---
+++ Minggu_10/app/src/main/java/com/example/jsonmoviedb/ApiEndpoint.java
@@ -0,0 +1,13 @@
+package com.example.jsonmoviedb;
+
+public class ApiEndpoint {
+
+    public static String BASEURL = "https://api.themoviedb.org/3/";
+    public static String APIKEY =  "api_key=13e46f5e5736bfd93514cfe34af54882";
+    public static String LANGUAGE = "&language=en-US";
+    public static String SEARCH_MOVIE = "search/movie?";
+    public static String QUERY = "&query=";
+    public static String MOVIE_PLAYNOW = "movie/now_playing?";
+    public static String MOVIE_POPULAR = "discover/movie?";
+    public static String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
+}
